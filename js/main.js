0ebe0f5c33144265779878580738f3350edf8bf1
emptywork <22065214+EmptyWork@users.noreply.github.com>
2021-01-31 00:52:31 +0900

    v1.0.0-Alpha28

--- js/main.js
+++ js/main.js
@@ -27,3 +27,66 @@ const nextLomba = (_daftarLomba, _base) => {
 }
 
 window.setInterval(gantiLomba, 6000)
+
+
+// Javascript untuk navbar mobile
+
+let tombolMobile = document.querySelectorAll('.navbar__more')[0];
+let tombolTutup = document.querySelectorAll('.navbar__more')[1];
+let navbarLokasi = document.querySelector('nav');
+let navbarDiv = document.querySelector('.navbar__mobile')
+
+let submenuNavbarMobile = navbarDiv.querySelectorAll('.navbar__submenu')
+
+submenuNavbarMobile.forEach(sublinks => {
+    sublinks.addEventListener('click', e => {
+        let submenuActive = e.target.parentElement.parentElement
+        console.log(submenuActive)
+        if(submenuActive.classList.contains('open')) {
+            submenuActive.classList.remove('open')
+            submenuActive.nextElementSibling.classList.remove('open')
+            setTimeout(() => {
+                submenuActive.nextElementSibling.style.display = 'none'
+            }, 200)
+        } else {
+            submenuActive.nextElementSibling.style.display = 'block'
+            setTimeout(() => {
+                submenuActive.nextElementSibling.classList.add('open')
+            }, 200)
+            submenuActive.classList.add('open')
+        }
+        // e.target.parentElement.nextElementSibling
+    })
+})
+
+tombolMobile.addEventListener('click', e => {
+    // console.log(navbarLokasi);
+    if(tombolMobile.classList.contains('open')) {
+        tombolMobile.classList.remove('open')
+        document.body.classList.remove('menuOpen')
+        navbarDiv.classList.remove('open')
+    } else {
+        tombolMobile.classList.add('open')
+        document.body.classList.add('menuOpen')
+        navbarDiv.classList.add('open')
+    }
+})
+
+tombolTutup.addEventListener('click', e => {
+    // console.log(navbarLokasi);
+    if(tombolMobile.classList.contains('open')) {
+        tombolMobile.classList.remove('open')
+        document.body.classList.remove('menuOpen')
+        navbarDiv.classList.remove('open')
+    } else {
+        tombolMobile.classList.add('open')
+        document.body.classList.add('menuOpen')
+        navbarDiv.classList.add('open')
+    }
+})
+
+// testing area 
+
+// document.body.addEventListener('click', e => {
+//     console.log(e.target.classList)
+// })
